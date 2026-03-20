9af8b9a0a1ae5741363773e8a011d2b95255c6ef
Uliana Golubeva <onlilonly@mail.ru>
2026-03-20 17:03:42 +0200

    Реализована константа для фильтров

---
+++ src/constants/filters.ts
@@ -0,0 +1,14 @@
+export const FILTER_LABELS: Record<string, string> = {
+  android: "Android",
+  ios: "iOS",
+  design: "Дизайн",
+  management: "Менеджмент",
+  qa: "QA",
+  back_office: "Бэк-офис",
+  frontend: "Frontend",
+  hr: "HR",
+  pr: "PR",
+  backend: "Backend",
+  support: "Техподдержка",
+  analytics: "Аналитика",
+};
