a0890f086b8417e32d99b54c4808bff6d70b8148
Uliana Golubeva <onlilonly@mail.ru>
2026-03-21 15:33:39 +0200

    Реализованы варианты страниц с ошибками(не загрузилось вообще/ничего не найдено)

---
+++ src/pages/ErrorPage/ErrorPage.styled.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const ErrorContainer = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`;
+
+export const Emoji = styled.img`
+    display: block;
+    object-fit: cover;
+    object-position; center;
+    font-size: 56px;
+`;
+
+export const Title = styled.h2`
+    font-size: 1.063rem;
+    font-weight: 600;
+    color: #050510;
+    line-height: 22px;
+`;
+
+export const Subtitle = styled.h3`
+    font-size: 1rem;
+    font-weight: 400;
+    color: #97979B;
+    line-height: 20px;
+`;
+
+export const RetryButton = styled.button`
+    font-size: 1rem;
+    font-weight: 600;
+    color: #6534FF;
+    line-height: 20px;
+    background: none;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,42 @@
+import {
+    ErrorContainer,
+    Emoji,
+    Title,
+    Subtitle,
+    RetryButton,
+} from "./ErrorPage.styled";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import ufo from "../../assets/flying-saucer_1f6f8.png";
+import glass from '../../assets/left-pointing-magnifying-glass_1f50d.png'
+
+export const ErrorBlockPage: React.FC = () => {
+    const navigate = useNavigate();
+    const onRetry = () => {
+        navigate("/users");
+    };
+
+    return (
+        <ErrorContainer>
+            <Emoji src={ufo}></Emoji>
+
+            <Title>Какой-то сверхразум все сломал</Title>
+
+            <Subtitle>Постараемся быстро починить</Subtitle>
+
+            <RetryButton onClick={onRetry}>Попробовать снова</RetryButton>
+        </ErrorContainer>
+    );
+};
+
+export const NoOneFoundBlock: React.FC = () => {
+  return (
+        <ErrorContainer>
+            <Emoji src={glass}></Emoji>
+
+            <Title>Мы никого не нашли</Title>
+
+            <Subtitle>Попробуй скорректировать запрос</Subtitle>
+        </ErrorContainer>
+    );
+}
