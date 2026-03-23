fbc56e59eb8e12e65f050b7a856090b95f38990d
Uliana Golubeva <onlilonly@mail.ru>
2026-03-23 04:36:11 +0200

    App перемещен, исправлены импорты, удален базовый путь vite

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from "react";
-import { useAppSelector } from "./hooks/useAppSelector";
-import { useAppDispatch } from "./hooks/useAppDispatch";
+import { useAppSelector } from "../hooks/useAppSelector";
+import { useAppDispatch } from "../hooks/useAppDispatch";
 import { ThemeProvider } from "styled-components";
-import { HomePage } from "./pages/HomePage/HomePage";
-import { UserPage } from "./pages/UserPage/UserPage";
-import { ErrorBlockPage } from "./pages/ErrorPage/ErrorPage";
-import { darkTheme, lightTheme } from "./styles/theme";
-import { setTheme } from "./store/themeSlice";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { UserPage } from "../pages/UserPage/UserPage";
+import { ErrorBlockPage } from "../pages/ErrorPage/ErrorPage";
+import { darkTheme, lightTheme } from "../styles/theme";
+import { setTheme } from "../store/themeSlice";
 
 const App = () => {
     const theme = useAppSelector((state) => state.theme.theme);
